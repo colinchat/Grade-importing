a0119cbee0fbf88eb890b8984f1e4d0fb20319e7
Colin Chatfield <cchatfie@uwaterloo.ca>
2021-01-21 15:37:46 -0500

    fixed variable name issues and config file issue

--- grade_import.py
+++ grade_import.py
@@ -1,33 +1,37 @@
+import json
 import csv
 from datetime import datetime, timedelta, timezone
 
-# learn export template
-lFile = open('ME 101 - Winter 2021_GradesExport.csv', 'r')
+configFile = open('config.json', 'r')
+configData = json.load(configFile)
+configFile.close()
+
+# learnData export template
+lFile = open(configData["file"]["learn_format_export"], 'r')
 lReader = csv.DictReader(lFile)
 lFieldnames = lReader.fieldnames
 lFieldnames.append("due date UTC")
-learn = list(lReader)
+learnData = list(lReader)
 lFile.close()
 
 # extensions file import (single sheet unique to assignment)
-eFile = open('Extensions.csv', 'r')
+eFile = open(configData["file"]["extensions"], 'r')
 eReader = csv.DictReader(eFile)
 extensions = list(eReader)
 eFile.close()
 
-# --------------original due dates (change these each assignment)---------------------
-tuesDate = datetime.strptime("2021-1-13 21:30:59 +0000", '%Y-%m-%d %H:%M:%S %z')
-wedDate = datetime.strptime("2021-1-14 21:30:59 +0000", '%Y-%m-%d %H:%M:%S %z')
-# ------------------------------------------------------------------------------------
+# original due dates for each lab section
+tuesDate = datetime.strptime(configData["dates"]["tues_due_datetime"], '%Y-%m-%d %H:%M:%S %z')
+wedDate = datetime.strptime(configData["dates"]["wed_due_datetime"], '%Y-%m-%d %H:%M:%S %z')
 
 print("Extensions found:")
-for line in learn:
+for line in learnData:
     extTime = timedelta(hours = 0)
-    # check all rows in extensions.csv for each line in learn
+    # check all rows in extensions.csv for each line in learnData
     # (assume extensions.csv is unique to the assignment)
     for row in extensions:
         if '#' + row["User ID"] == line["Username"]:
-            extTime = timedelta(hours = 72)
+            extTime = timedelta(hours = int(configData["dates"]["ext_length_in_hours"]))
             print("[SYSTEM]" + row["User ID"] + " was given an extension.")
     # match the lab session to due date, add any extensions if found
     if line["ME101_chulls_pmteerts_1211_LAB"] == "242 Tuesday Lab":
@@ -36,23 +40,23 @@ for line in learn:
         line["due date UTC"] = wedDate + extTime
     else:
         line["due date UTC"] = "ERROR: lab session not found"
-# now have list of adjusted due dates appended to learn doc in last column (after end of line indicator, so OK to import?)
+# now have list of adjusted due dates appended to learnData doc in last column (after end of line indicator, do not write to import file)
 
-# marmoset file import
-mFile1 = open('project-Assignment 1 - Tuesday lab-grades.csv', 'r')
+# marmosetData file import
+mFile1 = open(configData["file"]["marmoset_tues"], 'r')
 mReader1 = csv.DictReader(mFile1)
 mFieldnames = mReader1.fieldnames
-marmosetT = list(mReader1)
+marmosetDataT = list(mReader1)
 mFile1.close()
-mFile2 = open('project-Assignment 1 - Wednesday lab-grades.csv', 'r')
+mFile2 = open(configData["file"]["marmoset_wed"], 'r')
 mReader2 = csv.DictReader(mFile2)
-marmosetW = list(mReader2)
+marmosetDataW = list(mReader2)
 mFile2.close()
 
-marmoset = marmosetT + marmosetW
+marmosetData = marmosetDataT + marmosetDataW
 
 def getDueDate(student):
-    for row in learn:
+    for row in learnData:
         if '#' + student == row["Username"]:
             return row["due date UTC"]
     return "FAILED"
@@ -61,11 +65,11 @@ print("\nLate submissions found:")
 newFile = open('test.csv', 'w', newline='')
 writer = csv.DictWriter(newFile, mFieldnames)
 writer.writeheader()
-writer.writerows(marmoset)
+writer.writerows(marmosetData)
 newFile.close()
 
-# calculate adjusted marmoset grades
-for row in marmoset:
+# calculate adjusted marmosetData grades
+for row in marmosetData:
     subDate = datetime.fromtimestamp(int(row["UTC"]) / 1000, tz = timezone.utc)
     dueDate = getDueDate(row["classAccount"])
     if getDueDate(row["classAccount"]) != "FAILED" and subDate > dueDate:
@@ -80,11 +84,11 @@ for row in marmoset:
             print("[MARM]Late penalty of 100% applied to " + row["classAccount"] + ":\n   submitted on  ", subDate, "\n   with due date ", dueDate)
 
 # take highest grade from each student
-for row1 in marmoset:
+for row1 in marmosetData:
     student = row1["classAccount"]
     highestGrade = float(row1["total"])
     bestSubDate = datetime.fromtimestamp(int(row1["UTC"]) / 1000, tz = timezone.utc)
-    for row2 in marmoset:
+    for row2 in marmosetData:
         if student == row2["classAccount"]:
             row2["classAccount"] = "to be removed";
             if highestGrade < float(row2["total"]):
@@ -93,23 +97,23 @@ for row1 in marmoset:
     row1["classAccount"] = student
     row1["total"] = highestGrade
     row1["UTC"] = (bestSubDate - datetime(1970, 1, 1, tzinfo = timezone.utc)) / timedelta(seconds=1)
-marmoset = list(filter(lambda d: d["classAccount"] != "to be removed", marmoset))
+marmosetData = list(filter(lambda d: d["classAccount"] != "to be removed", marmosetData))
 
-# crowdmark file import
-cFile1 = open('a1_prog_tue_lab-marks.csv', 'r')
+# crowdmarkData file import
+cFile1 = open(configData["file"]["crowdmark_tues"], 'r')
 cReader1 = csv.DictReader(cFile1)
 cFieldnames = cReader1.fieldnames
-crowdmarkT = list(cReader1)
+crowdmarkDataT = list(cReader1)
 cFile1.close()
-cFile2 = open('a1_prog_wed_lab-marks.csv', 'r')
+cFile2 = open(configData["file"]["crowdmark_wed"], 'r')
 cReader2 = csv.DictReader(cFile2)
-crowdmarkW = list(cReader2)
+crowdmarkDataW = list(cReader2)
 cFile2.close()
 
-crowdmark = crowdmarkT + crowdmarkW
+crowdmarkData = crowdmarkDataT + crowdmarkDataW
 
-# calculate adjusted crowdmark grades
-for row in crowdmark:
+# calculate adjusted crowdmarkData grades
+for row in crowdmarkData:
     student = row["Email"].split("@")[0]
     if row["Submitted At"]:
         subDate = datetime.strptime(row["Submitted At"], '%Y-%m-%d %H:%M:%S %Z')
@@ -133,17 +137,17 @@ for row in crowdmark:
             print("[CROWD]Late penalty of 100% applied to " + row["Email"].split("@")[0] + ":\n   submitted on  ", subDate, "\n   with due date ", dueDate)
 print("\nErrors found: ")
 
-# combine marks from marmoset and crowdmark lists
-for line in learn:
+# combine marks from marmosetData and crowdmarkData lists
+for line in learnData:
     student = line["Username"]
     mMark = 1000; 
     cMark = 1000;
     # check both lists
-    for row in marmoset:
+    for row in marmosetData:
         if '#' + row["classAccount"] == student:
             mMark = row["total"]
-    for row in crowdmark:
-        if '#' + row["Email"].split("@")[0] == student and row["Total After Penalty"]: # crowdmark leaves blank space if not graded
+    for row in crowdmarkData:
+        if '#' + row["Email"].split("@")[0] == student and row["Total After Penalty"]: # crowdmarkData leaves blank space if not graded
             cMark = row["Total After Penalty"] 
     # check that marks were found before recording sum
     if mMark == 1000 and cMark == 1000:
@@ -158,28 +162,28 @@ for line in learn:
         line["Assignment 1 Points Grade <Numeric MaxPoints:13 Weight:10 Category:Assignments CategoryWeight:10>"] = float(mMark) + float(cMark)
 
 # write final data to new file
-newFile = open('grade_import.csv', 'w', newline='')
+newFile = open(configData["file"]["output_name"], 'w', newline='')
 writer = csv.DictWriter(newFile, lFieldnames)
 writer.writeheader()
-writer.writerows(learn)
+writer.writerows(learnData)
 newFile.close()
 
 # other compiled lists written to excel for debugging
-newFile = open('marmoset_compiled.csv', 'w', newline='')
+newFile = open('marmosetData_compiled.csv', 'w', newline='')
 writer = csv.DictWriter(newFile, mFieldnames)
 writer.writeheader()
-writer.writerows(marmoset)
+writer.writerows(marmosetData)
 newFile.close()
 
-newFile = open('crowdmark_compiled.csv', 'w', newline='')
+newFile = open('crowdmarkData_compiled.csv', 'w', newline='')
 writer = csv.DictWriter(newFile, cFieldnames)
 writer.writeheader()
-writer.writerows(crowdmark)
+writer.writerows(crowdmarkData)
 newFile.close()
 
-newFile = open('learn_compiled.csv', 'w', newline='')
+newFile = open('learnData_compiled.csv', 'w', newline='')
 writer = csv.DictWriter(newFile, lFieldnames)
 writer.writeheader()
-writer.writerows(learn)
+writer.writerows(learnData)
 newFile.close()
 
